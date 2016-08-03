6ef2101edda9139c8b5872f9d623988ec2f79f92
tom68 <tom68@3354ce84-6216-0410-ad3d-afc617165c18>
2016-08-03 19:58:48 +0000

    SFEmediatomb.spec: add --deaemon in mediatomb.sh SMF startscript

--- spec-files-extra/trunk/ext-sources/mediatomb.sh
+++ spec-files-extra/trunk/ext-sources/mediatomb.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-#newtask -p mediatomb /usr/bin/mediatomb --home /etc/mediatomb $*
-/usr/bin/mediatomb --home /etc/mediatomb $*
+#newtask -p mediatomb /usr/bin/mediatomb --home /etc/mediatomb --daemon $* 
+/usr/bin/mediatomb --home /etc/mediatomb --daemon $* &
